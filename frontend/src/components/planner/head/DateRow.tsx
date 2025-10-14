3cde2ea73afef30d10423820b53cbb29b14d9a17
Julian Bath <Julzso23@users.noreply.github.com>
2025-10-14 17:50:36 +0100

    Slightly improved planner head performance

--- frontend/src/components/planner/head/DateRow.tsx
+++ frontend/src/components/planner/head/DateRow.tsx
@@ -1,21 +1,23 @@
-import { DateTime, DateTimeUnit, Interval } from 'luxon'
+import { DateTime, DateTimeUnit } from 'luxon'
 import { DateRowCell } from './DateRowCell'
 
-export function DateRow({ dateUnit, interval }: {
+export function DateRow({ dateUnit, dateCells }: {
   dateUnit: DateTimeUnit,
-  interval: Interval,
+  dateCells: [number, number][],
 }) {
-  const days: (DateTime | null)[] = interval.splitBy({ day: 1 }).map(day => day.start)
-  const years: Map<number, number> = days.map(day => day?.startOf('year').toMillis()).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1 / days.length), new Map())
-  const months: Map<number, number> = days.map(day => day?.startOf('month').toMillis()).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1 / days.length), new Map())
-  const weeks: Map<number, number> = days.map(day => day?.startOf('week').toMillis()).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1 / days.length), new Map())
+  const getDateString = (millis: number) => {
+    const date: DateTime = DateTime.fromMillis(millis)
+    switch (dateUnit) {
+      case 'year': return date.toLocaleString({ year: 'numeric' })
+      case 'month': return date.toLocaleString({ month: 'short' })
+      case 'week': return 'W ' + date.weekNumber
+      case 'day': return date.toLocaleString({ day: '2-digit' })
+    }
+  }
 
   return (
     <div className="flex flex-row border-b last:border-none border-gray-800 first:rounded-t-lg overflow-hidden">
-      { dateUnit == 'year' && Array.from(years).map(year => <DateRowCell widthPercent={year[1] * 100} key={year[0]}>{ DateTime.fromMillis(year[0]).toLocaleString({ year: 'numeric' }) }</DateRowCell>) }
-      { dateUnit == 'month' && Array.from(months).map(month => <DateRowCell widthPercent={month[1] * 100} key={month[0]}>{ DateTime.fromMillis(month[0]).toLocaleString({ month: 'short' }) }</DateRowCell>) }
-      { dateUnit == 'week' && Array.from(weeks).map(week => <DateRowCell widthPercent={week[1] * 100} key={week[0]}>W { DateTime.fromMillis(week[0]).weekNumber }</DateRowCell>) }
-      { dateUnit == 'day' && days.map(day => <DateRowCell widthPercent={(1 / days.length) * 100} key={day?.toUnixInteger()}>{ day?.toLocaleString({ day: '2-digit' }) }</DateRowCell>) }
+      { dateCells.map(date => <DateRowCell widthPercent={date[1] * 100} key={date[0]}>{ getDateString(date[0]) }</DateRowCell>) }
     </div>
   )
 }
--- frontend/src/components/planner/head/PlannerHead.tsx
+++ frontend/src/components/planner/head/PlannerHead.tsx
@@ -1,16 +1,19 @@
-import { DateTimeUnit, Interval } from "luxon";
+import { DateTime, DateTimeUnit, Interval } from "luxon";
 import { DateRow } from "./DateRow";
 
 export function PlannerHead({ interval, zoomLevel }: {
   interval: Interval,
   zoomLevel: DateTimeUnit,
 }) {
+  const days: (DateTime | null)[] = interval.splitBy({ day: 1 }).map(day => day.start)
+  const getDateCells = (dateUnit: DateTimeUnit) => Array.from(days.map(day => day?.startOf(dateUnit).toMillis()).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1 / days.length), new Map()))
+
   return (
-    <div className="w-full flex flex-col sticky top-0 z-10 bg-slate-950 rounded-lg border border-gray-800 overflow-hidden drop-shadow-lg">
-      { <DateRow dateUnit="year" interval={interval} /> }
-      { (zoomLevel == 'month' || zoomLevel == 'day') && <DateRow dateUnit="month" interval={interval} /> }
-      { zoomLevel == 'week' && <DateRow dateUnit="week" interval={interval} /> }
-      { zoomLevel == 'day' && <DateRow dateUnit="day" interval={interval} /> }
+    <div className="w-full flex flex-col sticky top-0 z-10 bg-slate-950 rounded-lg border border-gray-800 overflow-hidden shadow-lg">
+      { <DateRow dateUnit="year" dateCells={getDateCells('year')} /> }
+      { (zoomLevel == 'month' || zoomLevel == 'day') && <DateRow dateUnit="month" dateCells={getDateCells('month')} /> }
+      { zoomLevel == 'week' && <DateRow dateUnit="week" dateCells={getDateCells('week')} /> }
+      { zoomLevel == 'day' && <DateRow dateUnit="day" dateCells={getDateCells('day')} /> }
     </div>
   )
 }
