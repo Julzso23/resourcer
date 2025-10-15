a4ee3568fa722d41cfe99f1b54db243b44398b34
Julian Bath <Julzso23@users.noreply.github.com>
2025-10-15 16:36:48 +0100

    Started adding allocation dragging

--- frontend/src/components/planner/body/Allocation.tsx
+++ frontend/src/components/planner/body/Allocation.tsx
@@ -1,8 +1,56 @@
-export function Allocation({ left, width }: {
-  left: number,
-  width: number,
+import { Ref, useCallback, useEffect, useRef, useState } from "react"
+import { AllocationModel } from "../Planner"
+import { DateTime, Interval } from "luxon"
+
+export function Allocation({ allocation, interval }: {
+  allocation: AllocationModel,
+  interval: Interval,
 }) {
+  const ref: Ref<HTMLDivElement> = useRef(null)
+
+  const [dragging, setDragging] = useState(false)
+
+  const allocationLeft = (allocationInterval: Interval) => (Math.max(allocationInterval.start!.diff(interval.start!).as('days'), 0) / interval.length('days')) * 100 + '%'
+  const allocationWidth = (allocationInterval: Interval) => ((allocationInterval.intersection(interval)?.length('days') || 0) / interval.length('days')) * 100 + '%'
+  const [intervalOverride, setIntervalOverride] = useState<Interval | undefined>(undefined)
+  const [leftOffset, setLeftOffset] = useState<number>(0)
+
+  const onMouseDown = useCallback((event: React.MouseEvent) => {
+    setDragging(true)
+    const rowWidth: number = ref.current!.parentElement!.getBoundingClientRect().width
+    setLeftOffset(
+      event.clientX -
+      ref.current!.getBoundingClientRect().x -
+      Math.min((rowWidth / interval.length('days')) * (intervalOverride || allocation.interval).start!.diff(interval.start!).as('days'), 0))
+  }, [setDragging, setLeftOffset, ref, interval, allocation, intervalOverride])
+
+  const onMouseMove = useCallback((event: MouseEvent) => {
+    if (!dragging) return
+    const rowX: number = ref.current!.parentElement!.getBoundingClientRect().x
+    const rowWidth: number = ref.current!.parentElement!.getBoundingClientRect().width
+    const start: DateTime = interval.start!.plus({ days: (interval.length('days') / rowWidth) * (event.clientX - rowX - leftOffset) }).plus({ days: 0.5 }).startOf('day')
+    setIntervalOverride(Interval.after(start, allocation.interval.toDuration()))
+  }, [dragging, leftOffset, setIntervalOverride, allocation, interval, ref])
+
+  const onMouseUp = useCallback(() => {
+    setDragging(false)
+  }, [setDragging, intervalOverride, setIntervalOverride])
+
+  useEffect(() => {
+    document.body.addEventListener('mousemove', onMouseMove)
+    document.body.addEventListener('mouseup', onMouseUp)
+    return () => {
+      document.body.removeEventListener('mousemove', onMouseMove)
+      document.body.removeEventListener('mouseup', onMouseUp)
+    }
+  }, [onMouseMove])
+
   return (
-    <div className="absolute border top-1/8 h-3/4" style={{ left: left + '%', width: width + '%' }}></div>
+    <div
+      ref={ref}
+      className={`absolute border top-1/8 h-3/4 bg-green-950 border-emerald-950 rounded-sm shadow-md box-border select-none ${dragging ? 'shadow-black' : ''}`}
+      style={{ left: allocationLeft(intervalOverride || allocation.interval), width: allocationWidth(intervalOverride || allocation.interval) }}
+      onMouseDown={onMouseDown}>
+    </div>
   )
 }
--- frontend/src/components/planner/body/AllocationRow.tsx
+++ frontend/src/components/planner/body/AllocationRow.tsx
@@ -8,13 +8,11 @@ export function AllocationRow({ interval, columnWidths, allocations }: {
   allocations: AllocationModel[],
 }) {
   const shouldShowAllocation = (allocation: AllocationModel) => interval.overlaps(allocation.interval)
-  const allocationLeft = (allocation: AllocationModel) => (Math.max(allocation.interval.start!.diff(interval.start!).as('days'), 0) / interval.length('days')) * 100
-  const allocationWidth = (allocation: AllocationModel) => (allocation.interval.intersection(interval)!.length('days') / interval.length('days')) * 100
 
   return (
-    <div className="flex flex-row h-10 mt-1 first:mt-0 bg-gray-900 drop-shadow-md relative">
+    <div className="flex flex-row h-10 mt-1 first:mt-0 bg-gray-900 shadow-md relative select-none">
       { columnWidths.map((columnWidth, index) => <div key={index} style={{ width: columnWidth * 100 + '%' }} className="border-r last:border-none border-dashed border-gray-800"></div>) }
-      { allocations.map(allocation => shouldShowAllocation(allocation) && <Allocation key={allocation.name} left={allocationLeft(allocation)} width={allocationWidth(allocation)} />) }
+      { allocations.map(allocation => shouldShowAllocation(allocation) && <Allocation key={allocation.name} allocation={allocation} interval={interval} />) }
     </div>
   )
 }
