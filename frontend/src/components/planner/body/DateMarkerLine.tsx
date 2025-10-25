eb91be7544d79383752ce1be5c6ae2efe6b46627
Julian Bath <Julzso23@users.noreply.github.com>
2025-10-25 21:37:13 +0100

    Added today marker line

---
+++ frontend/src/components/planner/body/DateMarkerLine.tsx
@@ -0,0 +1,11 @@
+import { DateTime, Interval } from "luxon";
+
+export function DateMarkerLine({ date, interval }: {
+  date: DateTime,
+  interval: Interval,
+}) {
+  const left: number = (date.diff(interval.start!).as('days') / interval.length('days')) * 100
+  return (
+    <div className="border-l border-white border-dashed absolute h-full opacity-50" style={{ left: left + '%', display: interval.contains(date) ? 'block' : 'none' }}></div>
+  )
+}
--- frontend/src/components/planner/body/PlannerBody.tsx
+++ frontend/src/components/planner/body/PlannerBody.tsx
@@ -1,6 +1,7 @@
 import { DateTime, DateTimeUnit, Interval } from "luxon";
 import { ProjectAllocationsModel } from "../Planner";
 import { AllocationRowContainer } from "./AllocationRowContainer";
+import { DateMarkerLine } from "./DateMarkerLine";
 
 export function PlannerBody({ interval, zoomLevel, projectAllocations }: {
   interval: Interval,
@@ -12,8 +13,11 @@ export function PlannerBody({ interval, zoomLevel, projectAllocations }: {
     .map(date => date[1] / days.length)
 
   return (
-    <div className="w-full flex flex-col gap-1 min-h-4 overflow-hidden">
+    <div className="w-full flex flex-col gap-1 min-h-4 overflow-hidden relative">
       { projectAllocations.map((project, index) => <AllocationRowContainer key={index} project={project} interval={interval} columnWidths={columnWidths} />) }
+      <div className="absolute top-0 right-0 bottom-0 left-0 xl:ml-81 lg:ml-61 ml-41 box-border pointer-events-none">
+        <DateMarkerLine date={DateTime.now()} interval={interval} />
+      </div>
     </div>
   )
 }
