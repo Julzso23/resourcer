39041abed941b51979a35b92db1d7914257f7991
Julian Bath <Julzso23@users.noreply.github.com>
2025-10-25 20:44:17 +0100

    Improved resize handle interaction visuals

--- frontend/src/components/planner/body/AllocationResizeHandle.tsx
+++ frontend/src/components/planner/body/AllocationResizeHandle.tsx
@@ -8,11 +8,11 @@ export function AllocationResizeHandle({ right = false, onPointerDown, onPointer
 }) {
   return (
     <div
-      className={`h-full w-fit p-1 cursor-ew-resize opacity-0 group-hover:opacity-100 ${right ? 'ml-auto' : ''}`}
+      className={`h-full w-fit p-1 cursor-ew-resize opacity-0 group-hover:opacity-50 active:opacity-90 ${right ? 'ml-auto' : ''}`}
       onPointerDown={onPointerDown}
       onPointerMove={onPointerMove}
       onPointerUp={onPointerUp}>
-      <div className="bg-[repeating-linear-gradient(90deg,rgba(255,255,255,0.5),rgba(255,255,255,0.5)_1px,transparent_1px,transparent_3px)] h-full w-2"></div>
+      <div className="bg-[repeating-linear-gradient(90deg,rgba(255,255,255,1),rgba(255,255,255,1)_1px,transparent_1px,transparent_3px)] h-full w-2"></div>
     </div>
   )
 }
