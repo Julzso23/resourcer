645386242384bb1d03785ec0e3971c423e46cfa3
Julian Bath <Julzso23@users.noreply.github.com>
2025-11-26 20:23:06 +0000

    Fixed button type

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -4,10 +4,11 @@ export function Button({ children, onClick, negative = false, type }: {
   children?: ReactNode,
   onClick?: MouseEventHandler,
   negative?: boolean,
-  type?: string,
+  type?: 'submit' | 'reset' | 'button' | undefined,
 }) {
   return (
     <button
+      type={type}
       className={`rounded-lg border border-gray-800 shadow-sm px-2 py-1 cursor-pointer hover:shadow-black/80
         active:shadow-none ${negative ? 'bg-red-900' : 'bg-slate-900'} ${negative ? 'hover:bg-red-800' : 'hover:bg-gray-900'}`}
       onClick={onClick}>
