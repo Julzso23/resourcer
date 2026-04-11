03171ec7d80e951ea8167fbfafc169e668c65c4b
Julian Bath <Julzso23@users.noreply.github.com>
2026-04-11 22:56:05 +0100

    Added calendar date selection

--- frontend/src/components/calendar/Calendar.tsx
+++ frontend/src/components/calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon'
+import { DateTime, Interval } from 'luxon'
 import { CalendarDate } from './CalendarDate'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,11 +7,26 @@ import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 export function Calendar({
   showWeekends = true,
   size = 'sm',
+  selectionSize = 'day',
+  selectionType = 'single',
+  selectedDate,
+  selectedDateRange,
+  onDateSelect,
+  onDateRangeSelect,
 }: {
   showWeekends?: boolean,
   size?: 'sm' | 'md' | 'lg',
+  selectionSize?: 'day' | 'halfDay',
+  selectionType?: 'single' | 'range',
+  selectedDate?: DateTime,
+  selectedDateRange?: Interval,
+  onDateSelect?: (date: DateTime) => void,
+  onDateRangeSelect?: (range: Interval) => void,
 }) {
   const [viewDate, setViewDate] = useState(DateTime.now().startOf('month').startOf('week'))
+  const [selectedStartDate, setSelectedStartDate] = useState<DateTime | undefined>(undefined)
+  const [hoveredDateRange, setHoveredDateRange] = useState<Interval | undefined>(undefined)
+
   const dates = Array.from({ length: 42 }, (_, i) => viewDate.plus({ days: i }))
   const titles = dates.slice(0, showWeekends ? 7 : 5).map(date => date.toFormat('ccc'))
   const highlightedMonth = viewDate.plus({ weeks: 2 }).startOf('month')
@@ -41,7 +56,51 @@ export function Calendar({
 
   const isCurrentMonth = (date: DateTime) => date.startOf('month').equals(highlightedMonth)
 
-  return <div className='bg-slate-950 p-2 rounded-md w-fit select-none'>
+  const singleDateClick = useCallback((date: DateTime) => {
+    if (!selectedDate?.equals(date)) {
+      onDateSelect?.(date)
+    } else {
+      onDateSelect?.(undefined)
+    }
+  }, [onDateSelect, selectedDate])
+
+  const selectionSizeToDuration = () => {
+    switch (selectionSize) {
+      case 'day': return { hours: 24 }
+      case 'halfDay': return { hours: 12 }
+    }
+  }
+
+  const rangeDateClick = useCallback((date: DateTime) => {
+    if (selectedStartDate == null) {
+      onDateRangeSelect?.(undefined)
+      setSelectedStartDate(date)
+    } else {
+      if (selectedStartDate > date) {
+        onDateRangeSelect?.(Interval.fromDateTimes(date, selectedStartDate.plus(selectionSizeToDuration())))
+      } else {
+        onDateRangeSelect?.(Interval.fromDateTimes(selectedStartDate, date.plus(selectionSizeToDuration())))
+      }
+      setSelectedStartDate(undefined)
+      setHoveredDateRange(undefined)
+    }
+  }, [onDateRangeSelect, selectedDateRange, selectedStartDate])
+
+  const datePointerEnter = useCallback((date: DateTime) => {
+    if (selectedStartDate != null) {
+      if (selectedStartDate > date) {
+        setHoveredDateRange(Interval.fromDateTimes(date, selectedStartDate.plus(selectionSizeToDuration())))
+      } else {
+        setHoveredDateRange(Interval.fromDateTimes(selectedStartDate, date.plus(selectionSizeToDuration())))
+      }
+    }
+  }, [hoveredDateRange, selectedStartDate])
+
+  const datePointerLeave = useCallback((date: DateTime) => {
+    setHoveredDateRange(undefined)
+  }, [])
+
+  return <div className='bg-slate-950 p-2 rounded-md w-fit select-none shadow-md'>
     <h1 className='font-semibold text-lg'>{highlightedMonth.toFormat('MMMM yyyy')}</h1>
     <div className='flex flex-row gap-1' ref={scrollRef}>
       <div className={`grid ${showWeekends ? 'grid-cols-7' : 'grid-cols-5'} gap-1 ${width()}`}>
@@ -51,7 +110,13 @@ export function Calendar({
           </div>
         ))}
         {dates.map((date, i) => (
-          (showWeekends || !date.isWeekend) && <CalendarDate key={i} date={date} currentMonth={isCurrentMonth(date)} />
+          (showWeekends || !date.isWeekend) &&
+            <CalendarDate key={i} date={date} currentMonth={isCurrentMonth(date)} selectionSize={selectionSize}
+              selectedDateRange={selectedDateRange}
+              hoveredDateRange={hoveredDateRange}
+              onClick={selectionType === 'single' ? singleDateClick : rangeDateClick} selectedDate={selectedDate}
+              onPointerEnter={datePointerEnter}
+              onPointerLeave={datePointerLeave} />
         ))}
       </div>
 
--- frontend/src/components/calendar/CalendarDate.tsx
+++ frontend/src/components/calendar/CalendarDate.tsx
@@ -1,16 +1,54 @@
-import { DateTime } from 'luxon'
+import { DateTime, Interval } from 'luxon'
 
 export function CalendarDate({
   date,
   currentMonth,
+  selectionSize,
+  selectedDate,
+  selectedDateRange,
+  hoveredDateRange,
+  onClick,
+  onPointerEnter,
+  onPointerLeave,
 }: {
   date: DateTime,
   currentMonth: boolean,
+  selectionSize?: 'day' | 'halfDay',
+  selectedDate?: DateTime,
+  selectedDateRange?: Interval,
+  hoveredDateRange?: Interval,
+  onClick?: (date: DateTime) => void,
+  onPointerEnter?: (date: DateTime) => void,
+  onPointerLeave?: (date: DateTime) => void,
 }) {
-  return <div className={`rounded-md aspect-square flex justify-center items-center cursor-pointer
+  return <div className={`rounded-md aspect-square flex justify-center items-center cursor-pointer relative
+    overflow-hidden shadow-sm
     ${date.equals(DateTime.now().startOf('day')) ? 'border border-white' : ''}
     ${currentMonth ? 'opacity-100' : 'opacity-50'}
     ${date.isWeekend ? 'bg-gray-800 hover:bg-gray-700' : 'bg-slate-700 hover:bg-slate-600'}`}>
-    { date.day }
+
+    {selectionSize === 'day' && <>
+      <div className={`w-full h-full hover:bg-blue-900 absolute left-0
+        ${selectedDate?.equals(date) || selectedDateRange?.contains(date) ? 'bg-blue-800' : ''}
+        ${hoveredDateRange?.contains(date) ? 'bg-blue-900' : ''}`}
+        onClick={() => onClick?.(date)} />
+    </>}
+
+    {selectionSize === 'halfDay' && <>
+      <div className={`w-1/2 h-full hover:bg-blue-900 absolute left-0
+        ${selectedDate?.equals(date) || selectedDateRange?.contains(date) ? 'bg-blue-800' : ''}
+        ${hoveredDateRange?.contains(date) ? 'bg-blue-900' : ''}`}
+        onClick={() => onClick?.(date)}
+        onPointerEnter={() => onPointerEnter(date)}
+        onPointerLeave={() => onPointerLeave(date)} />
+      <div className={`w-1/2 h-full hover:bg-blue-900 absolute right-0
+        ${selectedDate?.equals(date.plus({ hours: 12 })) || selectedDateRange?.contains(date.plus({ hours: 12 })) ? 'bg-blue-800' : ''}
+        ${hoveredDateRange?.contains(date.plus({ hours: 12 })) ? 'bg-blue-900' : ''}`}
+        onClick={() => onClick?.(date.plus({ hours: 12 }))}
+        onPointerEnter={() => onPointerEnter(date.plus({ hours: 12 }))}
+        onPointerLeave={() => onPointerLeave(date.plus({ hours: 12 }))} />
+    </>}
+
+    <span className='z-10 pointer-events-none'>{ date.day }</span>
   </div>
 }
